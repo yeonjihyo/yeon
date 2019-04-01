e99046313a850fab9c1584128513bac891d9a201
yeonjihyo <jihyo828>
2019-04-01 16:57:33 +0900

    5일차 배열예제5

---
+++ hello/src/Day5/Ex5.java
@@ -0,0 +1,62 @@
+package Day5;
+
+public class Ex5 {
+
+	public static void main(String[] args) {
+		// 향상된 for문
+		/*
+		 * 1.배열의 모든 값을들 확인한다
+		 * 2.배열의 값을 수정할때에는 사용불가
+		 * for(자료형변수명:배열이름){
+		 * 		실행문;
+		 * }
+		 * 자료형은 배열과 같은 자료형을 써야한다
+		 * */
+		/*int []num = new int[]{1,2,3,4,5};
+		for(int tmp: num){
+			System.out.print(tmp + " ");
+		}*/
+		
+		
+		/*int []num = new int[]{1,2,3,4,5};
+		boolean find =false;
+		int search = 6;
+		for(int tmp: num){
+			if(tmp == search){
+				find=true;
+				break;
+			}
+		}
+		if (find){
+			System.out.print("있음");
+		}else{
+			System.out.print("없음");
+		}
+*/
+		
+		
+		
+		
+		
+		
+		/* 배열정리
+		 * 배열 -같은자료형+같은의미
+		 * -시작번지는 0부터 배열의크기-1번지까지
+		 * -배열선언
+		 * 자료형[]배열이름 =new자료형[배열의크기];
+		 * -배열은 주로 반복문과사용
+		 * =>번지를 접근할때 조심해야한다 :ArrayIndexOutOfBoundsException
+		 * -향상된for문을 쓸 수 있다.
+		 * -배열내용검색, 정렬하기
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		
+		
+		
+		
+	}
+
+}

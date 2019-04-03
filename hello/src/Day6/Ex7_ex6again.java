e830eca73ee496b6e33382d65797453d05b15ac6
yeonjihyo <jihyo828>
2019-04-03 16:46:23 +0900

    6일차 ex7 수정

--- hello/src/Day6/Ex7_ex6again.java
+++ hello/src/Day6/Ex7_ex6again.java
@@ -4,7 +4,7 @@ public class Ex7_ex6again {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int max = 45, min = 1;//max가 7보다 작으면 배열 생성에 실패
+		int max = 10, min = 1;//max가 7보다 작으면 배열 생성에 실패
 		int [] arr= new int[7];
 		int [] arr2 = new int [6];
 		//int [] arr= null;//배열 생성에 실패
@@ -18,8 +18,20 @@ public class Ex7_ex6again {
 		}else{
 			System.out.println("배열 생성에 실패");
 		}
-
+		//당첨등수를 출력하는 코드를 작성해보새요
+		//1등 : 보너스번호를 제외한 번호 6개가 일치
+		//2등 : 보너스번호를 포함한 번호 6개가 일치
+		//3등 : 보너스번호를 제외한 번호 5개가 일치
+		//4등 : 보너스번호를 제외한 번호 4개가 일치
+		//5등 : 보너스번호를 제외한 번호 3개가 일치
+		//꽝 : 2개이하
+		int r= rank(arr,arr2);
+		printRank(r);
 	}
+    
+	
+	
+	
 	/* 기능 : 최소값(min)과 최댓값(max)가 주어지면 최솟값과 최댓값 사이의 임의의 정수를
 	 * 알려주는 메소드
 	 * 매개변수 : 최솟값, 최댓값=>int min, int max
@@ -119,6 +131,57 @@ public class Ex7_ex6again {
 		}
 		return true;
 	}
+	/* 기능 : 두 배열이 주어지면 두배열에 같은 원소가 몇개 있는지 알려주는 메소드
+	 * 매개변수 :두 배열 => int []arr1, int []arr2
+	 * 리턴타입 : 갯수=> int
+	 * 메소드명 compareArr
+	 * */
+	
+	public static int compareArr(int []arr1, int []arr2){
+		int cnt=0; //두 배열의 원소중 일치하는 갯수
+		int size = 0; //두 배열중 작은 배열의 길이를 저장
+		if (arr1.length > arr2.length){
+			size = arr2.length;
+		}else{
+			size = arr1.length;
+		}
+		//두 배열에서 일치하는 원소의 갯수를 구하는 과정
+		for(int i=0;i<size;i++){
+			if(contain(arr2,arr1[i],size)){
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+		/* 기능 : 당첨배열과 자동생성배열이 주어지면 몇등인지 알려주는 메소드 (단 꽝은 -1)
+		 * 매개변수 :두 배열 => int []lotto, int []auto
+		 * 리턴타입 : 등수=> int 
+		 * 메소드명 rank
+		 * */
+		public static int rank(int []lotto, int []auto){
+			int cnt = compareArr(lotto, auto);
+			switch(cnt){
+			case 6 : return 1;
+			case 5 : 
+				switch(contain2(auto,lotto[lotto.length-1],auto.length)){
+				case -1 :
+					return 3;
+				default:
+					return 2 ;
+				}
+			case 4 : return 4;
+			case 3 : return 5;
+			default:return -1;
+			}
+		}
+		public static void printRank(int r){
+			if (r == -1){
+				System.out.println("꽝");
+			}else{
+				System.out.println(r + "등");
+			}
+		}
+	
 	/*
 	 * 과정 
 	 * 배열을 생성

64989915875082a1a418ae4143f73da876e1f1cc
yeonjihyo <jihyo828>
2019-04-05 14:50:18 +0900

    8일차 메소드예제3수정

--- hello/src/Day8/Ex4_methodEx3.java
+++ hello/src/Day8/Ex4_methodEx3.java
@@ -61,7 +61,7 @@ public class Ex4_methodEx3 {
 	 * 반복문안에서 리턴을 항경우 조심해여한다
 	 * 조건문을 통해 리턴할 경우 조건문이 거짓인 경우룰 고려해야한다
 	 * 반복문 안에서 리턴을 할 경우 반복문 조건이 거짓이 되어 반복문이 실행되지 않을 경우를 고려해야한다*/
-	public static int test1(){
+	/*public static int test1(){
 		int r = 0;
 		for(int i=1;i<=9;i++){
 			r+=i;
@@ -70,23 +70,23 @@ public class Ex4_methodEx3 {
 		//반복문이 무조건 실행되서 리턴이 되는 경우라 하더라도 자바 컴파일러는
 		//세세한 내용까지 검사하지 않기 때문에 반복문이 종료됐을 경우 리턴도 설정해야한다
 		//return 0; 이렇게 추가해줘야 에러가 안뜸
-	}	
-	public static int test(int num){ //우리가보기에는 문제 없지만컴파일러는else일경우도 생각하기 때문에 오류가뜨는거임
+	}*/	
+	/*public static int test(int num){ //우리가보기에는 문제 없지만컴파일러는else일경우도 생각하기 때문에 오류가뜨는거임
 		if (num>=0) return 1;
 		else if (num<0) return -1; //else return -1; 이렇게 써야 에러가 사라짐
 		
-		}
+	}*/
 	
 	
 	
 	
-	public static int abs(int abs){
+	/*public static int abs(int abs){
 		int num = abs;
 		if(num<=0){
 			num = -abs;//리턴이없어서 오류
 		}else{
 			return abs;
-		}
+		}*/
 	/*
 	 * }
 		return num;
@@ -94,7 +94,7 @@ public class Ex4_methodEx3 {
 	이렇게 고칠수있다
 	*/
 	
-	}
+	/*}*/
 }
 
 

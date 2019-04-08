b2761853b5c9615741d45710b44ef51fc4d64789
yeonjihyo <jihyo828>
2019-04-08 16:26:16 +0900

    10일차 사각형

---
+++ hello/src/Day10/Rect.java
@@ -0,0 +1,31 @@
+package Day10;
+
+
+
+public class Rect extends Shape  {
+	private int area;
+	
+	public int getArea() {
+		return area;
+	}
+	//setter를 필요하지 않다.
+	//외부에서만 데이터를 접근할 수 있도록 하는 것이기 떄문에 예를 들면 4*8은 32인데 30을 주라고 할수있으니까???
+	public Rect(){}
+	public Rect (int x,int y, int w, int h){
+		super(x,y,w,h);//부모클래스의 생성자를 호출
+		area =w*h;
+	}
+	@Override
+	public void print(){
+		super.print();
+		System.out.println("넓이: "+ area);
+	}
+
+	//가로와 세로를 재설정할경우 
+	public void resize(int width, int height){
+		this.width=width;
+		this.height=height;
+		area= width*height;
+	}
+	
+}

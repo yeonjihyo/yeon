0b77ae3dd23f6920542a2f6f8091d70e2f63e6b9
yeonjihyo <jihyo828>
2019-04-16 16:44:13 +0900

    16일차 컬렉션프레임웍 설명

---
+++ hello/src/day16/Ex.java
@@ -0,0 +1,36 @@
+package day16;
+
+public class Ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ * 컬렉션 프레임웍을 사용할 수 있다.
+컬렉션 프레임웍의 종류와 특성을 이해할 수 있다.
+
+list, set , map
+list :순서 o ,중복 o
+set :중복 불가능해서 순서유지 불가능
+map: <key, value>
+key :중복불가능
+value :중복가능
+
+중복체크는 어떻게?
+1.해당객체의 equals와 hashCode메소드를 확인
+2.없으면 부모클래스의 equals를 확인
+
+==> 클래스 선언시 equals를 오버라이딩 하지 않으면 객체의 중복확인을 주소로 한다.
+==>필요하면 클래스 선언시 equals를 오버라이딩 해야한다.
+
+스택 : 역순,메소드 호출, 스택은 클래스
+큐 :  순서대로 ,  메세지큐,  프린트 출력관리 큐는 인터페이스
+
+일반적으로 인터페이스는 객체를 생성할 수 없지만 
+해당 인터페이스를 구현한 클래스를 이용하여 객체를 생성할 수 있다.
+
+
+
+ * */
+	}
+
+}

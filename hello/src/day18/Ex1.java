efb64723601f78b5ad24fb68b79a7247042414b8
yeonjihyo <jihyo828>
2019-04-18 16:53:56 +0900

    18일차 설명

---
+++ hello/src/day18/Ex1.java
@@ -0,0 +1,45 @@
+package day18;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 컬렉션 프레임웍 : 
+		 * 
+		 * collection :하나의 객체를 저장, Iterator를 제공
+		 * list : 중복허용 =>순서저장,get()을 통해 원하는 번지에 직접접근이 가능
+		 * set : 중복x=> 순서보장을 하지 않는다
+		 * 		직접접근이 불가능 ==> Iterator를 이용해 접근
+		 * map : 두개의 객체를 저장, key와 value
+		 * 		key : 중복불가  value : 중복
+		 * 
+		 * list=>ArrayList,LinkedList
+		 * set=>HashSet
+		 * map=>HashMap
+		 * 
+		 * 자료구조
+		 * stack : LIFO 메소드호출스택
+		 * Queue : FIFO 프린터출력순서,메세지 큐
+		 * 
+		 * */
+		
+		
+		
+		
+		/*
+		 * 지네릭클래스
+		 * -컬렉션프레임웍에서 제공하는 다양한 지네릭 클래스들은 잘 사용하기 위해서
+		 * -지네릭 클래스용어
+		 * -지네릭 클래스 타입변수 제한하는 방법
+		 * -와일드 카드
+		 * 
+		 * 열거형
+		 * -제한된 값들을 가지는 멤버들을 효율적으로 관리하기 위해
+		 * 
+		 * */
+
+	}
+
+}
+

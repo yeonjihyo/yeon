8828f03bca1d4fccca7155856a1fb9d249988a58
yeonjihyo <jihyo828>
2019-05-01 09:53:58 +0900

    신입생학생수출력쿼리문수정

--- SQL/SQL_EX/0430ex1.sql
+++ SQL/SQL_EX/0430ex1.sql
@@ -1,4 +1,7 @@
-/*신입생들의 학생수를 출력하는 쿼리문*/
+/*university 데이터베이스에서 신입생들의 학생수를 출력하는 쿼리문*/
 SELECT count(*)
 FROM university.student
 where student.student_num>=2019000000;
+/* student_num이 varchar일떄
+where student.student_num like '2019%'
+*/

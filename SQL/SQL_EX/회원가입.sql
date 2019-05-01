9a82dc1f6ab224fb81f19ac6ea86e7a82bdef8f8
yeonjihyo <jihyo828>
2019-05-01 16:41:07 +0900

    SQL-회원가입

---
+++ SQL/SQL_EX/회원가입.sql
@@ -0,0 +1,4 @@
+/*website 데이터베이스에 있는 회원가입하는 쿼리문을 작성하세요*/
+insert into user(user_id,user_pw,user_name,user_email,user_nick,user_phone,user_address)
+values('fff111','ff123','홍길동','f1@naver.com','길동이','01012341234','청주');
+SELECT * FROM website.user;

e1c615d1df8e4b827dc6681be48ab0a76b96d0fe
yeonjihyo <jihyo828>
2019-05-20 16:50:17 +0900

    0520.JAVA-합구하는예제

---
+++ hello/src/ex/Ex1.java
@@ -0,0 +1,17 @@
+package ex;
+
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*1~10까지의 합을 구하는 코드를 작성하세요 */
+		int sum=0;
+		for(int i=1;i<=10;i++){
+			sum+=i;
+		}
+		System.out.println(sum);
+
+	}
+
+}

5c821dfa50aebb37bdfe6101b24a0cd9db75d845
yeonjihyo <jihyo828>
2019-05-20 16:50:53 +0900

    0520.JAVA-배열예제

---
+++ hello/src/ex/Ex2.java
@@ -0,0 +1,19 @@
+package ex;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		
+		/*1~6을 배열에 저장하는 코드를 작성하세요 */
+		int arr[]=new int[6];
+		 for (int i=0;i<arr.length;i++){
+			 arr[i]=i+1;
+		 }
+		 for (int i=0;i<arr.length;i++){
+			 System.out.print(arr[i]+" ");
+		 }
+	 }
+		
+}
+
+

b78c9f1727754fa25b6494058c3a8e55c84c4444
yeonjihyo <jihyo828>
2019-06-24 16:45:49 +0900

    0624제이쿼리예제-마우스이용한박스크기조절 js

---
+++ html/day17/js/exam2.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('div').mouseenter(function(){
+		$(this).css('border-color','red');
+		$(this).animate({width:'100px',height:'100px'},1000)
+	})
+	$('div').mouseout(function(){
+		$(this).animate({width:'50px',height:'50px'},1000)
+		$(this).css('border-color','black');
+	})
+});

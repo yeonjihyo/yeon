ef3f9c3b417d462f32bdcd0621648b75fd59a9a7
yeonjihyo <jihyo828>
2019-06-25 16:45:02 +0900

    0625제이쿼리예제-더보기 js

---
+++ html/day18/js/add3.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('.more').click(function(){
+		//var str='<div class="contents"></div><div class="contents"></div><div class="contents"></div>';
+		$('.box').append('<div class="contents"></div><div class="contents"></div><div class="contents"></div>');
+	})
+});

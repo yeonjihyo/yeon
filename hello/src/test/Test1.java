6f029f9f012dc082e5650c4325b7ccf3003da025
yeonjihyo <jihyo828>
2019-07-02 14:16:10 +0900

    자바테스트예제-변수출력하기

---
+++ hello/src/test/Test1.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1.정수형 변수에 10을 저장 후 콘솔에 출력하는 코드를 작성하세요
+		/*Scanner scan=new Scanner(System.in);
+		int num=scan.nextInt();
+		System.out.println(num);*/
+		int num=10;
+		System.out.println(num);
+	}
+
+}

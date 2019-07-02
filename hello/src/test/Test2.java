b7bce67471033f60ecd13c6f8db56c7be52cb304
yeonjihyo <jihyo828>
2019-07-02 14:16:22 +0900

    자바테스트예제-합

---
+++ hello/src/test/Test2.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//2.1부터 10까지의 합을 구하는 코드를 작성하세요
+		int sum = 0;
+		for(int i=1;i<=10;i++){
+			sum+=i;
+		}
+		System.out.println("1부터 10까지의 합은 "+sum);
+	}
+
+}
